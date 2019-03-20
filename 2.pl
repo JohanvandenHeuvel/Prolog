2f1f147b0d7303a46c73219dc38db604ce2b1e63
Johan van den Heuvel <johanheuvel5@gmail.com>
2019-03-20 12:38:45 +0100

    3c
    finished up to 3c

--- 2.pl
+++ 2.pl
@@ -1,17 +1,27 @@
+% Facts.
 leaf.
 
+% Nodes consist of two elements:
+% a node, which results in a recursion;
+% a leaf, which is a fact.
 node(X, Y) :-
     X,
     Y.
 
+% Term is a binaryTree if it consists of nodes and leafs.
 isBinaryTree(Term) :-
     Term.
 
+% Count the number of nodes.
 nnodes(leaf, 1).
 nnodes(node(X, Y), N) :-
-    nnodes(X, Sum1),
-    nnodes(Y, Sum2),
+    % Recurse for each member of the tree
+    nnodes(X, Sum1), nnodes(Y, Sum2),
+    % Add 1 for each recursion step, i.e. the depth
     N is Sum1 + Sum2 + 1.
 
+% Create a binary tree of depth N.
+makeBinary(N, Tree) :-
+    isBinaryTree(Tree).
 
     
---
+++ 3.pl
@@ -0,0 +1,23 @@
+% is the first argument a member of the second argument.
+member(X, [X|_]).
+member(X, [_|Y]) :-
+    member(X,Y).
+
+% Is the first argument an subset of the second argument.
+subset([],L).
+subset([X|L1],L) :-
+    member(X, L),
+    subset(L1,L).
+
+path(X, X, Graph, Visited, Path) :-
+    % We need to reverse the list as we append to the front.
+    reverse(List, [X|Visited]),
+    % Output the reversed list.
+    write(List).
+path(From, To, Graph, Visited, Path) :-
+    % Networks is bi-directional so we need the disjuntion.
+    (member(edge(From, X), Graph); member(edge(X, From), Graph)),
+    % We want to use an edge at most 1 time.
+    not(member(X, Visited)),
+    % Recursion into the next node.
+    path(X, To, Graph, [From|Visited], Path).   
