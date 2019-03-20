eee1dab3cc1785ae55854f688020baece7ebdea7
Johan van den Heuvel <johanheuvel5@gmail.com>
2019-03-20 13:23:41 +0100

    Auto stash before revert of "3c"

--- 3.pl
+++ 3.pl
@@ -11,13 +11,13 @@ subset([X|L1],L) :-
 
 path(X, X, Graph, Visited, Path) :-
     % We need to reverse the list as we append to the front.
-    reverse(List, [X|Visited]),
-    % Output the reversed list.
-    write(List).
+    reverse(Path, [X|Visited]).
 path(From, To, Graph, Visited, Path) :-
     % Networks is bi-directional so we need the disjuntion.
     (member(edge(From, X), Graph); member(edge(X, From), Graph)),
     % We want to use an edge at most 1 time.
     not(member(X, Visited)),
     % Recursion into the next node.
-    path(X, To, Graph, [From|Visited], Path).   
+    path(X, To, Graph, [From|Visited], Path).  
+    
+      
